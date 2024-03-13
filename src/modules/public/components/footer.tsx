4c97d13453ccb61abc6fa5efb661df617c8bae71
Joseph Samuel <mrsamd02@gmail.com>
2024-03-13 23:50:02 +0100

    .

--- src/modules/public/components/footer.tsx
+++ src/modules/public/components/footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
     >
       <Stack />
       <ul className=" w-full  md:w-2/6 mx-4 text-xl text-green-100 md:mx-auto flex flex-col px-4">
-        <h3 className="text-lg font-thin py-3 text-white">Links</h3>
+        <h3 className="text-lg font-bold py-3 text-white">Links</h3>
         <a className="py-1 font-thin " href={"/"}>
           Home
         </a>
@@ -17,32 +17,36 @@ const Footer = () => {
           About
         </a>
         <a className="py-1 font-thin " href={ROUTES.GALLERY}>
-          Gallery{" "}
+          Gallery
         </a>
         <a className="py-1 font-thin " href={ROUTES.BLOG}>
-          Blog{" "}
+          Blog
         </a>
         <a className="py-1 font-thin " href={ROUTES.ADMIN}>
-          Admin{" "}
+          Admin
         </a>
-        
-  <br/> <br/>
- <br/>
+
+        <br />
       </ul>
 
       <ul className="mx-auto w-full  text-xl md:w-2/6 flex flex-col px-4 text-green-100  ">
-        <h3 className=" text-lg font-thin py-3 text-white">Contacts</h3>
-        <span className="py-1 font-thin "> Email:greenassemblyinitiative@gmail.com</span>
+        <h3 className=" text-lg font-bold py-3 text-white">Contacts</h3>
+        <span className="py-1 font-thin "> Email:Info@greenassemblyinitiate.com.ng </span>
         <span className="py-1 font-thin "> Phone: +2347064391614</span>
         <span className="py-1 font-thin ">
           Office Address: Suit CB11, Apo shopping plaza, near living faith
           church, Durumi, Abuja
         </span>
         <br /> <br /> <br />
-        <span className="mx-1 text-base flex flex-row items-center ">
-          <MdCopyright size={22} />
-          <span className="mx-1 ">2024 Green Assembly</span> <span className="py-1 font-thin color-white text-sm"> Powered by Bright-Star Golden Empire</span>
-        </span>
+        <div className="w-full text-base flex flex-col ">
+          <span className=" flex flex-row ">
+            <MdCopyright size={22} />
+            2024 Green Assembly
+          </span>
+          <span className="py-1 font-thin color-white text-sm">
+            Powered by Bright-Star Golden Empire
+          </span>
+        </div>
       </ul>
     </div>
   );
